de89fa2eb95833a1cfe17d0252a3990d0fd066d0
Alan Chan <alan.c@gini.co>
2020-05-27 19:10:29 +0800

    Refactor tests

--- test/niddle/print_test.clj
+++ test/niddle/print_test.clj
@@ -4,18 +4,24 @@
    [niddle.print :refer :all]))
 
 (deftest extract-form-test
-  (is (= (extract-form {:code '(identity 1)}) '(identity 1)))
-  (is (= (extract-form {:code "(identity 1)"}) '(identity 1))))
+  (testing "Extracting string encoded forms"
+    (are [s expected] (= (extract-form {:code s}) expected)
+      "3" 3
+      ":key" :key
+      "(identity 1)" '(identity 1)))
+  (testing "Return decoded form as is"
+    (are [form] (= (extract-form {:code form}) form)
+      1 :key [1 2 3] '(identity 1)))
+  (is (extract-form {:code "#(identity %)"})
+      "Can extract forms with reader macro"))
 
 (deftest print-form?-test
-  (is (print-form? 1))
-  (is (print-form? :key))
-  (is (print-form? "str"))
-  (is (print-form? [1 2 3]))
-  (is (print-form? #{1 2 3}))
-  (is (print-form? {:a 5 :b 6}))
-  (is (print-form? 's))
-  (is (print-form? '(func arg))))
+  (testing "Forms that should be printed"
+    (are [form] (print-form? form)
+         1, :key, "str", [1 2 3], #{1 2 3}, {:a 5 :b 6}, 's, '(func arg)))
+  (testing "Forms that should not be printed"
+    (are [form] (not (print-form? form))
+         '(in-ns foo) '*ns*)))
 
 (comment
   (run-all-tests)
